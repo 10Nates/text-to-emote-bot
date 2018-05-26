512282de08df66523eafba8c43c3990e34b3a1a9
Nate B <nathanpbrust@gmail.com>
2018-05-25 23:11:31 -0500

    change in channel id

--- extras.js
+++ extras.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+﻿const Discord = require('discord.js');
 const bot = new Discord.Client();
 function getlength(number) {
     return number.toString().length;
@@ -6,13 +6,6 @@ function getlength(number) {
 ChanID = 'No ChanID specified';
 
 module.exports = {
-
-    logmsg: function logmsg(msg, message, bot) {
-        bot.channels.get('449044829578723341').send(`recieved: ${message.author.username} - ${message.content}`);
-        bot.channels.get('449044829578723341').send(`Sent: ${msg}`);
-        console.log(message.author.username + ' - ' + message.content);
-        console.log(`sent: ${msg}`);
-    },
 
     emojiconvert: function emojiconvert(msgstart) {
         msg1 = msgstart.replace(/a/g, '🅰');
@@ -64,22 +57,29 @@ module.exports = {
         return msg
     },
 
+    logmsg: function logmsg(msg, message, bot) {
+        bot.channels.get('449759068941189151').send(`recieved: ${message.author.username} - ${message.content}`);
+        bot.channels.get('449759068941189151').send(`Sent: ${msg}`);
+        console.log(message.author.username + ' - ' + message.content);
+        console.log(`sent: ${msg}`);
+    },
+
     runHive: function runHive(message, bot) {
         const argz = message.content.split(/ +/g);
         if (message.author.id === '186507006008360960') {
-            if (message.channel.id === '449043802485948418') {
+            if (message.channel.id === '449759045491097610') {
                 if (getlength(argz[0]) == 18) {
                     msg = message.content.replace(argz[0] + ' ', ''); /*.replace(/!!HIVEMINDovrd /g, '')*/
                     bot.channels.get(argz[0]).send(msg);
-                    bot.channels.get('449044829578723341').send(`recieved: ${message.author.username} - ${message.content}`);
-                    bot.channels.get('449044829578723341').send(`Sent: ${msg} to <#${argz[0]}>`);
+                    bot.channels.get('449759068941189151').send(`recieved: ${message.author.username} - ${message.content}`);
+                    bot.channels.get('449759068941189151').send(`Sent: ${msg} to <#${argz[0]}>`);
                     console.log(message.author.username + ' - ' + message.content);
                     console.log(`Sent: ${msg} to <#${argz[0]}>`);
                     ChanID = argz[0];
                 } else {
                     bot.channels.get(ChanID).send(message.content);
-                    bot.channels.get('449044829578723341').send(`recieved: ${message.author.username} - ${message.content}`);
-                    bot.channels.get('449044829578723341').send(`Sent: ${message.content} to <#${ChanID}>`);
+                    bot.channels.get('449759068941189151').send(`recieved: ${message.author.username} - ${message.content}`);
+                    bot.channels.get('449759068941189151').send(`Sent: ${message.content} to <#${ChanID}>`);
                     console.log(message.author.username + ' - ' + message.content);
                     console.log(`Sent: ${message.content} to <#${ChanID}>`);
                 }
